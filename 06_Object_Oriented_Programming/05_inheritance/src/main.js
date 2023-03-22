3a141291bb031bb474487aa0546c73713ecb6fdb
Adham Nugroho <adhamngroho9@gmail.com>
2023-03-23 05:26:18 +0700

    inheritance done

---
+++ 06_Object_Oriented_Programming/05_inheritance/src/main.js
@@ -0,0 +1,16 @@
+// Superclass
+class MailService {
+  constructor(sender) {
+    this.sender = sender;
+  }
+
+  sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+  }
+}
+
+// Subclass
+class WhatsAppService extends MailService {}
+
+// Subclass
+class EmailService extends MailService {}
---
+++ 06_Object_Oriented_Programming/05_inheritance/src/main2.js
@@ -0,0 +1,28 @@
+// Superclass
+class MailService {
+  constructor(sender) {
+    this.sender = sender;
+  }
+
+  sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+  }
+}
+
+// Subclass
+class WhatsAppService extends MailService {
+  sendBroadcastMessage(message, receivers) {
+    for (const receiver of receivers) {
+      this.sendMessage(message, receiver);
+    }
+  }
+}
+
+// Subclass
+class EmailService extends MailService {
+  sendDelayedMessage(message, receiver, delay) {
+    setTimeout(() => {
+      this.sendMessage(message, receiver);
+    }, delay);
+  }
+}
